5eef427f31b96de588db9e655db08386e5004152
sudakarvengatesan <sudakar1703@gmail.com>
2023-05-25 18:14:22 +0530

    First

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutusComponent } from './Main/aboutus/aboutus.component';
+import { EducationComponent } from './Main/education/education.component';
+import { ExperienceComponent } from './Main/experience/experience.component';
+import { SkillsComponent } from './Main/skills/skills.component';
+import { ContactusComponent } from './Main/contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutusComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,

be2b49713216c7486fb98b98562fc0355188813b
sudakarvengatesan <sudakar1703@gmail.com>
2023-05-26 14:26:58 +0530

    education added

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { AboutusComponent } from './Main/aboutus/aboutus.component';
-import { EducationComponent } from './Main/education/education.component';
-import { ExperienceComponent } from './Main/experience/experience.component';
-import { SkillsComponent } from './Main/skills/skills.component';
-import { ContactusComponent } from './Main/contactus/contactus.component';
+import { AboutusComponent } from './main/aboutus/aboutus.component';
+import { EducationComponent } from './main/education/education.component';
+import { ExperienceComponent } from './main/experience/experience.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { ContactusComponent } from './main/contactus/contactus.component';
 
 @NgModule({
   declarations: [
